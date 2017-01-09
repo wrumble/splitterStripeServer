6ab180d0033790aca3adfa65508d00c96328dc6e
wrumble <rumblewayne@gmail.com>
2017-01-09 16:16:22 +0000

    key

--- web.rb
+++ web.rb
@@ -9,7 +9,8 @@ Dotenv.load
 Stripe.api_key = ENV['STRIPE_TEST_SECRET_KEY']
 
 use Rack::Session::EncryptedCookie,
-  :secret => 'supersecretkeyforwaynessplitterteststripeserver' # Actually use something secret here!
+
+  :secret =>  ENV['SERVER_SECRET'] # Actually use something secret here!
 
 get '/' do
   status 200
