25cbd5cfc951f5a6bbe8995df640153dc69a19f5
wrumble <rumblewayne@gmail.com>
2017-01-10 00:06:02 +0000

    key

--- web.rb
+++ web.rb
@@ -52,17 +52,17 @@ post '/account/create' do
     @account = Stripe::Account.create(
       :legal_entity => {
                         :first_name => params[:first_name],
-                        :last_name => params[:last_name],
-                        :dob => {
-                                  :day => params[:day],
-                                  :month => params[:month]
-                                  # :year => params[:year]
-                                }
-                        },
-      # :tos_acceptance => {
-      #                     :date => Time.now.utc,
-      #                     :ip => request.ip
-      #                    },
+                        :last_name => params[:last_name]
+                        # :dob => {
+                        #           :day => params[:day],
+                        #           :month => params[:month],
+                        #           :year => params[:year]
+                        #         }
+      },
+      :tos_acceptance => {
+                          :date => Time.now.utc,
+                          :ip => request.ip
+      },
       :type => "individual",
       :country => params[:country],
       :managed => true
