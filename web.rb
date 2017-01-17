983b694e8c7eec39de78b27e949a9636d1c4a37f
wrumble <rumblewayne@gmail.com>
2017-01-17 17:19:54 +0000

    things

--- web.rb
+++ web.rb
@@ -54,12 +54,12 @@ post '/charge' do
   return "Bill splitter charged successfully."
 end
 
-get '/customer' do
-  authenticate!
-  status 200
-  content_type :json
-  @customer.to_json
-end
+# get '/customer' do
+#   authenticate!
+#   status 200
+#   content_type :json
+#   @customer.to_json
+# end
 
 post '/account/create' do
   begin
@@ -150,90 +150,90 @@ post '/account/id/save' do
   return account.to_json
 end
 
-post '/customer/sources' do
-  authenticate!
-  source = params[:source]
-
-  # Adds the token to the customer's sources
-  begin
-    @customer.sources.create({:source => source})
-  rescue Stripe::StripeError => e
-    status 402
-    return "Error adding token to customer: #{e.message}"
-  end
-
-  begin
-    charge = Stripe::Charge.create(
-      :amount => params[:amount], # this number should be in cents
-      :currency => params[:currency],
-      :customer => @customer.id,
-      :source => source,
-      :description => params[:description]
-    )
-  rescue Stripe::StripeError => e
-    status 402
-    return "Error creating charge: #{e.message}"
-  end
-
-  status 200
-  return "Charge successfully created"
-end
-
-post '/customer/default_source' do
-  authenticate!
-  source = params[:source]
-
-  # Sets the customer's default source
-  begin
-    @customer.default_source = source
-    @customer.save
-  rescue Stripe::StripeError => e
-    status 402
-    return "Error selecting default source: #{e.message}"
-  end
-
-  status 200
-  return "Successfully selected default source."
-end
-
-def authenticate!
-  # This code simulates "loading the Stripe customer for your current session".
-  # Your own logic will likely look very different.
-  return @customer if @customer
-  if session.has_key?(:customer_id)
-    customer_id = session[:customer_id]
-    begin
-      @customer = Stripe::Customer.retrieve(customer_id)
-    rescue Stripe::InvalidRequestError
-    end
-  else
-    begin
-      @customer = Stripe::Customer.create(:description => "iOS SDK example customer")
-    rescue Stripe::InvalidRequestError
-    end
-    session[:customer_id] = @customer.id
-  end
-  @customer
-end
-
-# This endpoint is used by the Obj-C example to complete a charge.
-post '/charge_card' do
-  # Get the credit card details submitted by the form
-  token = params[:stripe_token]
-
-  # Create the charge on Stripe's servers - this will charge the user's card
-  begin
-    charge = Stripe::Charge.create(
-      :amount => params[:amount], # this number should be in cents
-      :currency => params[:currency],
-      :card => token,
-      :description => params[:description]
-    )
-  rescue Stripe::StripeError => e
-    status 402
-    return "Error creating charge: #{e.message}"
-  end
-
-  status 200
-  return "Charge successfully created"
-end
+# post '/customer/sources' do
+#   authenticate!
+#   source = params[:source]
+#
+#   # Adds the token to the customer's sources
+#   begin
+#     @customer.sources.create({:source => source})
+#   rescue Stripe::StripeError => e
+#     status 402
+#     return "Error adding token to customer: #{e.message}"
+#   end
+#
+#   begin
+#     charge = Stripe::Charge.create(
+#       :amount => params[:amount], # this number should be in cents
+#       :currency => params[:currency],
+#       :customer => @customer.id,
+#       :source => source,
+#       :description => params[:description]
+#     )
+#   rescue Stripe::StripeError => e
+#     status 402
+#     return "Error creating charge: #{e.message}"
+#   end
+#
+#   status 200
+#   return "Charge successfully created"
+# end
+#
+# post '/customer/default_source' do
+#   authenticate!
+#   source = params[:source]
+#
+#   # Sets the customer's default source
+#   begin
+#     @customer.default_source = source
+#     @customer.save
+#   rescue Stripe::StripeError => e
+#     status 402
+#     return "Error selecting default source: #{e.message}"
+#   end
+#
+#   status 200
+#   return "Successfully selected default source."
+# end
+#
+# def authenticate!
+#   # This code simulates "loading the Stripe customer for your current session".
+#   # Your own logic will likely look very different.
+#   return @customer if @customer
+#   if session.has_key?(:customer_id)
+#     customer_id = session[:customer_id]
+#     begin
+#       @customer = Stripe::Customer.retrieve(customer_id)
+#     rescue Stripe::InvalidRequestError
+#     end
+#   else
+#     begin
+#       @customer = Stripe::Customer.create(:description => "iOS SDK example customer")
+#     rescue Stripe::InvalidRequestError
+#     end
+#     session[:customer_id] = @customer.id
+#   end
+#   @customer
+# end
+#
+# # This endpoint is used by the Obj-C example to complete a charge.
+# post '/charge_card' do
+#   # Get the credit card details submitted by the form
+#   token = params[:stripe_token]
+#
+#   # Create the charge on Stripe's servers - this will charge the user's card
+#   begin
+#     charge = Stripe::Charge.create(
+#       :amount => params[:amount], # this number should be in cents
+#       :currency => params[:currency],
+#       :card => token,
+#       :description => params[:description]
+#     )
+#   rescue Stripe::StripeError => e
+#     status 402
+#     return "Error creating charge: #{e.message}"
+#   end
+#
+#   status 200
+#   return "Charge successfully created"
+# end
