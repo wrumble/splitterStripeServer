05d75e72bd3d44d981fd4a1a0522d9ad5ceeb370
wrumble <rumblewayne@gmail.com>
2017-01-17 16:32:08 +0000

    things

--- web.rb
+++ web.rb
@@ -30,9 +30,9 @@ end
 
 post '/charge' do
   authenticate!
-
+  p params
   begin
-    charge = Stripe::Charge.create({
+    charge = Stripe::Charge.create(
       :amount => params[:amount],
       :currency => params[:currency],
       :application_fee => (params[:amount] * 0.01),
